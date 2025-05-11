b18018c18092f5c1991ab8a5a701ef2b2b2b08d8
tsukitenDev <kouta.tat.manten.100@gmail.com>
2025-05-11 22:00:26 +0900

    vfs semihostに対応

--- main/network/storage.cpp
+++ main/network/storage.cpp
@@ -5,7 +5,11 @@
 
 #include "esp_vfs.h"
 #include "esp_vfs_fat.h"
+#include "esp_vfs_semihost.h"
 
+#include "sdkconfig.h"
+
+static const char *TAG = "storage";
 
 // Mount path for the partition
 const char *base_path = "/spiflash";
@@ -13,6 +17,23 @@ const char *base_path = "/spiflash";
 // Handle of the wear levelling library instance
 static wl_handle_t s_wl_handle = WL_INVALID_HANDLE;
 
+
+#if CONFIG_WEB_DEPLOY_SEMIHOST
+esp_err_t init_fs(void)
+{
+    esp_err_t ret = esp_vfs_semihost_register(base_path);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to register semihost driver (%s)", esp_err_to_name(ret));
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+#endif
+
+
+
+#if CONFIG_WEB_DEPLOY_SPIFLASH
 esp_err_t init_fs(){
   ESP_LOGI("storage", "Mounting FAT filesystem");
   // To mount device we need name of device partition, define base_path
@@ -26,8 +47,9 @@ esp_err_t init_fs(){
 
   esp_err_t err = esp_vfs_fat_spiflash_mount_rw_wl(base_path, "storage", &mount_config, &s_wl_handle);
   if (err != ESP_OK) {
-      ESP_LOGE("storage", "Failed to mount FATFS (%s)", esp_err_to_name(err));
+      ESP_LOGE(TAG, "Failed to mount FATFS (%s)", esp_err_to_name(err));
       return ESP_FAIL;
   }
   return ESP_OK;
 }
+#endif
